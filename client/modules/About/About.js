af6208f7a018651ff7066e954c641b5657b2c9d7
Michał Matysiak <michalmatysiak02@gmail.com>
2018-07-09 12:46:29 +0200

    add evaluate, edit posts

---
+++ client/modules/About/About.js
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react';
+
+const About = () => {
+	return (
+		<div>
+			<h1>MERN - dodatkowa funkcjonalność</h1>
+			<a href="/">Back to main page</a>
+		</div>
+	)
+}
+
+export default About;
